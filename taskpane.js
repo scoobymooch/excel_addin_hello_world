5890d3ddda33959eef78af654ccce2fb271ea47f
scoobymooch <moochtm@hotmail.com>
2025-10-06 20:10:15 +0000

    .

---
+++ taskpane.js
@@ -0,0 +1,17 @@
+Office.onReady(() => {
+  Excel.run(async context => {
+    const sheet = context.workbook.worksheets.getActiveWorksheet();
+    sheet.onSelectionChanged.add(onSelectionChanged);
+    await context.sync();
+  });
+});
+
+async function onSelectionChanged(eventArgs) {
+  await Excel.run(async context => {
+    const range = context.workbook.getSelectedRange();
+    range.load("address, values");
+    await context.sync();
+    document.getElementById("cellData").innerText =
+      `Address: ${range.address}\nValue: ${range.values[0][0]}`;
+  });
+}
